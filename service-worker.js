54ffa7f63cbec3f73337dbd130cb6c19972eef85
Yurii Adamenko <skyey07@gmail.com>
2020-10-08 00:20:53 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/millionaire/precache-manifest.0e71ffb86786c382db3d900b79450bb4.js"
+  "/millionaire/precache-manifest.04787c3aeb62e2e20e54c3112c78937a.js"
 );
 
 self.addEventListener('message', (event) => {
