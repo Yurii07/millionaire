1e4e55780bf055e3a446a55a5ab6c441053a8a67
Yurii Adamenko <skyey07@gmail.com>
2020-10-07 21:29:29 +0300

    Updates

---
+++ precache-manifest.0e71ffb86786c382db3d900b79450bb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0cb451e63a79594ac32c8ca1d48ad7f",
+    "url": "/millionaire/index.html"
+  },
+  {
+    "revision": "4101aac2b8e6f91d0e00",
+    "url": "/millionaire/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db37ed7ad0ec2910fb74",
+    "url": "/millionaire/static/js/2.44704294.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/millionaire/static/js/2.44704294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4101aac2b8e6f91d0e00",
+    "url": "/millionaire/static/js/main.18f663bf.chunk.js"
+  },
+  {
+    "revision": "078e70291101148e5f5b",
+    "url": "/millionaire/static/js/runtime-main.70d563fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/millionaire/static/media/logo.5d5d9eef.svg"
+  }
+]);
