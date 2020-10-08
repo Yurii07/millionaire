74009401d63d6ee547f9c38ba7de6b5d4f724e24
Yurii07 <skyey07@gmail.com>
2020-10-08 18:02:40 +0300

    add drawer component & menu

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
 import classes from './Layout.module.css';
+import MenuToggle from "../../components/Navigation/MenuToggle/MenuToggle";
+import Drawer from "../../components/Navigation/Drawer/Drawer";
 
 class Layout extends Component {
+
+    state = {
+        menu: false
+    }
+
+    toggleMenuHandler = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
+
     render() {
         return (
             <div className={classes.Layout}>
 
+                <Drawer isOpen={this.state.menu}/>
 
+                <MenuToggle
+                    onToggle={this.toggleMenuHandler}
+                    isOpen={this.state.menu}
+                />
 
                 <main>
                     {this.props.children}
